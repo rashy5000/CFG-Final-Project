6847e343a768cd637db2423150dc2b46ed70932b
Tash <natasiazoe@gmail.com>
2018-04-25 20:35:52 +0100

    ready kind of

--- bathtunes.js
+++ bathtunes.js
@@ -1,10 +1,14 @@
 $(document).on("scroll", function(){
-  if
-    ($(document).scrollTop() > 100){
-    $("header").addClass("shrink");
+  if($(document).scrollTop() > 100)
+  {
+    $(".header").addClass("shrink");
+    $(".heading").addClass("shrink");
+    $(".ul").addClass("shrink");
   }
   else
   {
-    $("header").removeClass("shrink");
+    $(".header").removeClass("shrink");
+    $(".heading").removeClass("shrink");
+    $(".ul").removeClass("shrink");
   }
 });
